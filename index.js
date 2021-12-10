e79fb6b68448420f9831368f06aa53113b9e5f0e
monsieurtyb <tyb1516@yahoo.fr>
2021-12-10 10:58:10 +0100

    new branch

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 
 app.get('/', (req, res) => {
@@ -6,7 +7,16 @@ app.get('/', (req, res) => {
     res.end();
 });
 
+app.get('/hello/:name', (req, res) => {
+    res.write(`Hello, ${req.params.name}`);
+    res.end();
+});
+
+app.get('/goodbye', (req, res) => {
+    res.write('Goodbye');
+    res.end();
+});
+
 app.listen(3000, _ => {
     console.log(`App listening on http://localhost:3000`);
 });
-
